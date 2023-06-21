3d1ff93e37ff5a672d5da18196d38cbf1002231a
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-06-21 18:09:37 +0200

    Fix input container margins

--- input-number.js
+++ input-number.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var InputNumber = Object.getPrototypeOf(app).InputNumber = new app.Component("input-number");
     //InputNumber.debug = true;
     InputNumber.createdAt      = "2.0.0";
-    InputNumber.lastUpdate     = "2.0.0";
-    InputNumber.version        = "1";
+    InputNumber.lastUpdate     = "2.3.3";
+    InputNumber.version        = "1.0.1";
     // InputNumber.factoryExclude = true;
     // InputNumber.loadingMsg     = "This message will display in the console when component will be loaded.";
     // InputNumber.requires       = [];
