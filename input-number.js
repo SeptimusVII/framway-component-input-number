d84d1661d1eebd5e3f61c96d93f7ae531e40260a
SeptimusVII <vansteene.quentin@gmail.com>
2025-10-02 11:21:30 +0200

    improve display of buttons while the associated input has border-radius

--- input-number.js
+++ input-number.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var InputNumber = Object.getPrototypeOf(app).InputNumber = new app.Component("input-number");
     //InputNumber.debug = true;
     InputNumber.createdAt      = "2.0.0";
-    InputNumber.lastUpdate     = "2.5.0";
-    InputNumber.version        = "1.1.0";
+    InputNumber.lastUpdate     = "2.6.0";
+    InputNumber.version        = "1.1.1";
     // InputNumber.factoryExclude = true;
     // InputNumber.loadingMsg     = "This message will display in the console when component will be loaded.";
     // InputNumber.requires       = [];
